4f6238a59fa5fe5c7c73b6778dbb8f593b2ff78f
amiboko <56851141+amiboko@users.noreply.github.com>
2020-01-30 11:09:28 +0200

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -30,7 +30,7 @@ exports.run = async (client, message, args) => {
 
 exports.conf = {
   enabled: true,
-  aliases: ['מכור', 'שלם', 'העבר', 'פרגן', 'תרום'],
+  aliases: ['מכור', 'שלם', 'העבר', 'תרומה', 'תרום'],
   guildOnly: true,
   permLevel: 'User'
 }
