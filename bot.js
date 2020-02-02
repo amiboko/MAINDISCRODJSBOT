5afbc42f939ce7be86aed651cebf79f1417861bb
amiboko <56851141+amiboko@users.noreply.github.com>
2020-02-02 23:55:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,47 @@ if (Number(process.version.slice(1).split('.')[0]) < 10) throw new Error('NodeJS
 if (process.env.PREBOOT) eval(process.env.PREBOOT)
 require('dotenv').config()
 
+const serverStats = {
+  guildID: '583574396686434304',
+  totalUsersID: '673640435968638977',
+  memberCountID: '673640466134073344',
+  botCount: '673640480692764673'
+  }
+  
+  //______________________When someone joins the server___________________
+  client.on('guildMemberAdd', member => {
+  client.channels.get(serverStats.totalUsersID).setName(`סהכ משתמשים ${member.guild.memberCount}`);
+  client.channels.get(serverStats.memberCountID).setName(`משתמשים ${member.guild.members.filter(m => !m.user.bot).size}`);
+  client.channels.get(serverStats.botCount).setName(`בוטים ${member.guild.members.filter(m => m.user.bot).size}`);
+  });
+  //______________________When someone leaves the server__________________
+  client.on('guildMemberRemove', member => {
+  client.channels.get(serverStats.totalUsersID).setName(`סהכ משתמשים ${member.guild.memberCount}`);
+  client.channels.get(serverStats.memberCountID).setName(`משתמשים ${member.guild.members.filter(m => !m.user.bot).size}`);
+  client.channels.get(serverStats.botCount).setName(`בוטים ${member.guild.members.filter(m => m.user.bot).size}`)
+    });
+
+
+
+
+
+
+    var cron = require('node-cron');
+ 
+    cron.schedule('* * * * *', function(){
+      console.log('running a task every minute');
+
+  
+    });
+
+    
 const Discord = require('discord.js')
 const Enmap = require('enmap')
 const client = new Discord.Client({
   disableEveryone: true,
   disabledEvents: ['TYPING_START']
+
+  
 })
 
 const dblposer = require('dblposter')
@@ -54,48 +90,6 @@ require('./modules/_functions')(client)
 require('./modules/commands')(client)
 require('./modules/events')(client)
 // require('./modules/webhooks')(client)
-
-
-const serverStats = {
-guildID: '583574396686434304',
-totalUsersID: '673640435968638977',
-memberCountID: '673640466134073344',
-botCount: '673640480692764673'
-}
-
-//______________________When someone joins the server___________________
-client.on('guildMemberAdd', member => {
-client.channels.get(serverStats.totalUsersID).setName(`סהכ משתמשים ${member.guild.memberCount}`);
-client.channels.get(serverStats.memberCountID).setName(`משתמשים ${member.guild.members.filter(m => !m.user.bot).size}`);
-client.channels.get(serverStats.botCount).setName(`בוטים ${member.guild.members.filter(m => m.user.bot).size}`);
-});
-//______________________When someone leaves the server__________________
-client.on('guildMemberRemove', member => {
-client.channels.get(serverStats.totalUsersID).setName(`סהכ משתמשים ${member.guild.memberCount}`);
-client.channels.get(serverStats.memberCountID).setName(`משתמשים ${member.guild.members.filter(m => !m.user.bot).size}`);
-client.channels.get(serverStats.botCount).setName(`בוטים ${member.guild.members.filter(m => m.user.bot).size}`)
-});
-
-
-
-var CronJob=require('cron').CronJob;
-var cronJob1 = new CronJob({
-
-    cronTime: '00 */1 * * * *',
-    onTick: function () {
-
-      const embed = new Discord.RichEmbed()
-      .setColor(0x8644ba)
-      .setDescription('test')
-      .then(() => client.channels.get('673211967216812068').send(embed)
-   
-      )},
-    start: true,
-    runOnInit: false
-});
-
-
-
 
 
 client.on('presenceUpdate', (oldMember, newMember) => {
