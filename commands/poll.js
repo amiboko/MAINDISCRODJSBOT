ffd6ba4dcf14d7c6bd389cd5ce2793abd73a5fe8
amiboko <56851141+amiboko@users.noreply.github.com>
2020-03-15 22:09:00 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,40 +1,39 @@
 const Discord = require('discord.js')
-const colors = require('../lib/colors.json')
 
-exports.run = async (client, message, args, level) => {
-  const yesEmoji = '👍'
-  const noEmoji = '👎'
-  const emojis = message.client.emojis.get('640480680576417803')
-  const settings = client.getSettings(message.guild.id)
-  const input = message.content.split(`${settings.prefix}הצבעה `)
 
-  const embed = new Discord.RichEmbed()
-    .setTitle('🗳 הצבעה')
-    .setColor(colors.default)
-    .addField(`תצביע עם 👍 או ${noEmoji} או תוסיף אחד לטעמך`, input, true)
-    .setTimestamp()
+module.exports.run = async (bot, message, args, Invite) => {
 
-  if (args.length === 0) {
-    message.channel.send(`על מה תיהיה ההצבעה? \nלדוגמה, \`${settings.prefix}האם מוריס גיי?\``)
-  } else {
-    message.delete()
-    message.channel.send(embed).then(message => {
-      message.react(yesEmoji)
-        .then(() => message.react(noEmoji))
-    })
-  }
-}
+  let Invite = message.guild.channels.first().createInvite()
+  let Owner = message.author;
+  if(Owner.id !== "ID 1" && Owner.id !== "ID 2") return message.reply("Only the bot owner can use this command!") //you need to add your id and another one if you want
+ 
+  const id = args.shift();
+  const sayMessage = args.join(" ")
+  if(!sayMessage) return message.reply("Usage `dm + ID + your message`")
+  
 
-exports.conf = {
-  enabled: true,
-  aliases: ['שאל','הצבעה','סקר'],
-  guildOnly: true,
-  permLevel: 'User'
-}
+ let contact = new Discord.RichEmbed()
+ .setAuthor(Owner.username)
+ .setColor("00ff00")
+ .setTitle("New dm")
+ .addField("Response:", sayMessage)
+ .addField("Server message departure:", message.guild.name)
+ .setTimestamp()
 
-exports.help = {
-  name: 'סקר',
-  category: 'כלים',
-  description: 'משאל עם, סקר, הצבעה. קרא לזה איך שאתה רוצה',
-  usage: 'רשום סקר או שאל או הצבעה והוסף תוכן'
-}
+  bot.users.get(id).send(contact);
+
+  let chanemb = new Discord.RichEmbed()
+  .setColor("#00ff00")
+  .setDescription(`Message sent to <@${id}>`);
+
+  message.channel.send(chanemb).then(msg => {msg.delete(5000)});
+
+
+      message.delete();
+
+    }
+    module.exports.help = {
+      name: "dm",
+      description: 'Owner Only',
+      usage: 'dm <ID> <message>'
+    }
