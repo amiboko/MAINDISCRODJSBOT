61d835b31a3bbaef7ad9b8234d745ca421c71ddc
amiboko <56851141+amiboko@users.noreply.github.com>
2020-03-16 21:20:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,14 +16,14 @@ const config = {
   defaultSettings: {
     prefix: '',
   language: 'en-US',
-    modLogChannel: 'Reports',
+    modLogChannel: '𝓡𝓮𝓹𝓸𝓻𝓽𝓼',
     modRole: 'Moderator',
     adminRole: 'Administrator',
     muteRole: 'Muted',
     noPermissionNotice: 'true',
-    deniedChannel: 'Reports',
-		acceptedChannel: 'Reports',
-    welcomeChannel: 'Main',
+    deniedChannel: '𝓡𝓮𝓹𝓸𝓻𝓽𝓼',
+		acceptedChannel: '𝓡𝓮𝓹𝓸𝓻𝓽𝓼',
+    welcomeChannel: '𝓜𝓪𝓲𝓷',
     welcomeMessage: 'טוב שבאת {{mention}}',
     welcomeEnabled: 'true',
     pointsEnabled: 'true',
@@ -31,7 +31,7 @@ const config = {
     logChannelUpdates: 'true',
     logEmojiUpdates: 'false',
     logMemberUpdates: 'true',
-    starboardChannel: 'Reports'
+    starboardChannel: '𝓡𝓮𝓹𝓸𝓻𝓽𝓼'
   },
 
   permLevels: [
