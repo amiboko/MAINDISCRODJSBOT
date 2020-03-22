ef025d08382edf54d492d88d53dd46d2e90e763a
amiboko <56851141+amiboko@users.noreply.github.com>
2020-03-22 15:19:23 +0200

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -57,7 +57,7 @@ module.exports = (client, oldMember, newMember) => {
     	.setColor(colors.default)
       .setDescription(`<@${newMember.id}>` + ' 🎮 ' + `${newMember.presence.game.name}`)
       .addField('◀️', `${output}`, true)
-      .addField('▶️', `${newMember.roles}`, true)
+      .addField('▶️', `${outputNew}`, true)
       .setThumbnail(`${oldMember.user.displayAvatarURL}`)
     	
 
