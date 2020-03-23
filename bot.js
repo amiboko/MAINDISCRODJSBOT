39150e610e56c206665103f977df3e19ed996617
amiboko <56851141+amiboko@users.noreply.github.com>
2020-03-23 21:53:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -113,7 +113,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
   const oldGame = oldMember.presence.game && [0, 1].includes(oldMember.presence.game.type) ? true : false;
   const newGame = newMember.presence.game && [0, 1].includes(newMember.presence.game.type) ? true : false;
 
-  embed = new Discord.RichEmbed()
+  const embed = new Discord.RichEmbed()
   .setAuthor(':aea6b19d1ebb42b998d64136ff2ede45:')
   .setColor("#F74847")
   .setDescription(`${newMember.user}` +   ':49cf4ba1f4034ffc841493817797b739:'  + `${newMember.presence.game.name}`)
