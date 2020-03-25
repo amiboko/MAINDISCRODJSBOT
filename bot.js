0b6d00b9335318fff63b8dd945a686af8d199a30
amiboko <56851141+amiboko@users.noreply.github.com>
2020-03-25 21:51:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,9 +114,9 @@ client.on('presenceUpdate', (oldMember, newMember) => {
   const newGame = newMember.presence.game && [0, 1].includes(newMember.presence.game.type) ? true : false;
 
   const embed = new Discord.RichEmbed()
-  .setTitle('New Status' + '\n')
+  .setTitle('New Status')
   .setColor("#3498DB")
-  .setDescription(`${newMember.user}`  + '...' + '\n\n' + `${playingRole}` + '  ' + `${newMember.presence.game}` +  '  ' + '<a:itsmine:691725601966391387>')
+  .setDescription(`\n${newMember.user}`  + '\n\n' + `${playingRole}` + '  ' + `${newMember.presence.game}` +  '\xa0\xa0  ' + '<a:itsmine:691725601966391387>')
   .setThumbnail(`${oldMember.user.displayAvatarURL}`)
 
   if (!oldGame && newGame) {         
