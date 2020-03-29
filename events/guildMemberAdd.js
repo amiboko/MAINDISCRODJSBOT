f13fea78d55e5a958a5c1ee58aa5240a939c941c
amiboko <56851141+amiboko@users.noreply.github.com>
2020-03-30 00:02:14 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -54,7 +54,7 @@ module.exports = (client, member) => {
       const embed = new Discord.RichEmbed()
         .setAuthor('𝒲𝐸𝐿𝒞𝒪𝑀𝐸')
         .setColor(colors.green)
-        .setDescription(`\n<@${member.user.id}> שלום לך \n\n **סהכ משתמשים** \`${member.guild.memberCount}\``)
+        .setDescription(`\n <@${member.user.id}> שלום לך \n\n **סהכ משתמשים** \`${member.guild.memberCount}\`\n` + '<a:pica:693846940743499846>')
         .setThumbnail(`${member.user.displayAvatarURL}`)
       modLogChannel.send(embed)
  		}
