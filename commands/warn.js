86a3cc2eb881651ed898b3411ac8c510dd693646
amiboko <56851141+amiboko@users.noreply.github.com>
2020-05-05 21:44:16 +0300

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -2,33 +2,16 @@ const Discord = require('discord.js')
 const colors = require('../lib/colors.json')
 
 exports.run = async (client, message, args, level) => {
- 
+  if (message.member.voiceChannel) {
+    message.member.voiceChannel.join()
+    .then(connection => {
+        const dispatcher = connection.playFile('./img/aniroze.mp3');
+        dispatcher.on("end", end => {message.member.voiceChannel.leave()});
+    })
+    .catch(console.error);
+  }
+};
 
-    async function play(channel) {
-      await channel.join().then(async (connection) => {
-        let dispatcher = await connection.playFile('./img/aniroze.mp3');
-        await dispatcher.on('end', function () {
-          channel.leave();
-        });
-      });
-    }
-  
-    let timer = 1000;
-    client.ShowSuccess('בודק בדיקה');
-    message.guild.channels.forEach(async (channel) => {
-      if (channel.type == 'voice' && channel.members.size > 0) {
-        client.ShowSuccess('בדיקה 33 ' + channel.name + ' בדיקה 22 ' + channel.members.size + ' בדיקה 11', message.channel);
-        client.logger.log('בדיקה1 ' + channel.name + ' בדיקה 2 ' + channel.members.size + ' בדיקה 3');
-        setTimeout(function () {
-          play(channel);
-        }, timer);
-        timer = timer + 10000;
-      }
-    });
-    setTimeout(function () {
-      client.ShowSuccess('בדיקה');
-    }, timer);
-  };
 
 exports.conf = {
   enabled: true,
