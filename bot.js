73b4d2f807bc5a2c24d7fdded9dae4b326768d4d
amiboko <56851141+amiboko@users.noreply.github.com>
2020-05-06 17:32:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,6 +197,22 @@ client.on('presenceUpdate', (oldMember, newMember) => {
 });
 
 
+client.on('message', async message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === 'yes') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
+
+
 client.on('message', message => {
   
   if(message.content.includes('SHROUD'))
