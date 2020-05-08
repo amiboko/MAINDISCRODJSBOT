4d24f27cb9cb461130a0ea0831cb1ba515a89ad8
amiboko <56851141+amiboko@users.noreply.github.com>
2020-05-08 16:21:23 +0300

    Update where.js

--- commands/where.js
+++ commands/where.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args, level) => {
 
   async function play(voiceChannel) {
     await voiceChannel.join().then(async (connection) => {
-      let dispatcher = await connection.playFile('./img/botsound.mp3');
+      let dispatcher = await connection.playFile('./img/botsound.mp3', {volume: 0.9,});
       await dispatcher.on('end', function () {
         voiceChannel.leave();
       });
@@ -16,13 +16,13 @@ exports.run = async (client, message, args, level) => {
 
       let timer = 5000;
       if (!voiceChannel) return message.reply('**אם אתה קורא לעצמך גבר.. כנס לערוץ שיחה ותרשום את זה שוב**')
-      voiceChannel.join()
+      await voiceChannel.join()
       const embed2 = new Discord.RichEmbed()
       .setTitle('לי ולך יש פיטפוט קטן, אני כמה שניות מחבר מיקרופון, שים פול ווליום')
       .setColor("#FF0000")
       .setThumbnail(`${message.author.displayAvatarURL}`)
 
-      message.channel.send(message.author + embed2);
+      message.channel.send(embed2);
       setTimeout(function () {
         play(voiceChannel);
       }, timer);
@@ -47,20 +47,3 @@ exports.help = {
   description: 'מצטרף אליך לשיחה ומשמיע לך קצת כללים',
   usage: 'שתוק'
 }
-
-
-  
-//   if (!args[0]) return message.channel.send('You need to specify a channel ID')
-//   if (!client.channels.get(args[0])) return message.channel.send('Couldn\'t find a channel with that ID')
-//   const channel = client.channels.find(ch => ch.id === args[0])
-  
-//   const embed = new Discord.RichEmbed()
-//     .setColor(colors.default)
-//     .addField(`Guild: `, channel.guild.name)
-//     .addField(`Channel: `, channel.name)
-//     .addField(`Guild Owner: `, channel.guild.owner)
-//     .setTimestamp();
-  
-//   message.author.send(embed)
-// };
-
