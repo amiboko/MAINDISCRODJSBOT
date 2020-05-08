ddaa233caf5570ffdd924314e803dd7e99ce7ab5
amiboko <56851141+amiboko@users.noreply.github.com>
2020-05-08 17:33:40 +0300

    Update where.js

--- commands/where.js
+++ commands/where.js
@@ -10,13 +10,17 @@ exports.run = async (client, message, args, level) => {
       let dispatcher = await connection.playFile('./img/botsound.mp3', {volume: 0.9,});
       await dispatcher.on('end', function () {
         voiceChannel.leave();
+        setTimeout(function () {
+        }, timer - 5000);
       });
   });
   }
 
       let timer = 5000;
       if (!voiceChannel) return message.reply('**אם אתה קורא לעצמך גבר.. כנס לערוץ שיחה ותרשום את זה שוב**')
-       voiceChannel.join()
+       voiceChannel.join().setTimeout(() => {
+         
+       }, timer);
       const embed2 = new Discord.RichEmbed()
       .setTitle('לי ולך יש פיטפוט קטן, אני כמה שניות מחבר מיקרופון, שים פול ווליום')
       .setColor("#FF0000")
@@ -24,8 +28,8 @@ exports.run = async (client, message, args, level) => {
 
       message.channel.send(embed2);
 
-      setTimeout(function () {play(voiceChannel)}, timer);
-      setTimeout(function () {voiceChannel.leave()}, timer);
+
+      setTimeout(function () {play(voiceChannel);}, timer);
 };
 
 
