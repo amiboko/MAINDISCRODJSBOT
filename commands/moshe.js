27c59cbb0dd5df6acbf01b4c4a0a3d6ef3439d35
amiboko <56851141+amiboko@users.noreply.github.com>
2020-05-09 03:40:10 +0300

    Update moshe.js

--- commands/moshe.js
+++ commands/moshe.js
@@ -7,32 +7,34 @@ exports.run = async (client, message, args, level) => {
   
   async function play(voiceChannel) {
     await voiceChannel.join().then(async (connection) => {
-      let dispatcher = await connection.playFile('./img/aniroze.mp3' , {volume: 0.8,});
+      let dispatcher = await connection.playFile('./img/aniroze.mp3', {volume: 0.9,});
+     
       await dispatcher.on('end', function () { 
-
         message.channel.send("🎧 **?נו אז איך היה הקטע של משה השראוד** 🎧").then(
           sentEmbed => {
           sentEmbed.react("👍")
-          sentEmbed.react("👎")
-        
-          voiceChannel.leave();} );
+          sentEmbed.react("👎")      } );
+          setTimeout(function () { voiceChannel.leave(); }, 5000);
       });
-  });
+      });
   }
 
-  let timer = 10000;
+       let timer = 10000;
+  
        if (!voiceChannel) return message.reply('**אתה לא בערוץ שיחה איך אתה רוצה לשמוע בידיוק?**')
+       
         voiceChannel.join()
+       
       const embed2 = new Discord.RichEmbed()
       .setTitle('🎧 MASTERBOT-TUBE 🎧')
       .setColor("#3498DB")
-      .setDescription(`${message.author}` + '\n\n' + 'תתחבר לערוץ שיחה תוך 10 שניות' + '\n\n' + 'ואני בדרך אליך עם אחד הקטעים החזקים של **משה השראוד**')
+      .setDescription(`${message.author}` +'\xa0\xa0\xa0\xa0' + 'ברוך הבא לנגן שלי' + '\n\n' + 'תוך `10 שניות` תקבל את אחד הלהיטים של **משה השראוד**')
       .setThumbnail(`${message.author.displayAvatarURL}`)
       .setTimestamp()
       message.channel.send(embed2);
-      
+     
       setTimeout(function () { play(voiceChannel); }, timer);
-      setTimeout(function () { }, timer);
+
 };
 
 exports.conf = {
