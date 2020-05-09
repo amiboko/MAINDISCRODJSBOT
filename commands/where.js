682aa500e721b15390f8c3a6c8adf638b6585365
amiboko <56851141+amiboko@users.noreply.github.com>
2020-05-09 22:43:19 +0300

    Update where.js

--- commands/where.js
+++ commands/where.js
@@ -1,46 +1,54 @@
-const Discord = require('discord.js');
-const colors = require('../lib/colors.json');
+const Discord = require('discord.js')
+const colors = require('../lib/colors.json')
 
-exports.run = async (client, message, args, level) => { 
+exports.run = async (client, message, args, level) => {
 
   const voiceChannel = message.member.voiceChannel
-
+  message.delete();
+  
   async function play(voiceChannel) {
     await voiceChannel.join().then(async (connection) => {
-      let dispatcher = await connection.playFile('./img/botsound.mp3');
-      dispatcher.setVolume(0.5);
-      await dispatcher.on('end', function () {
-        voiceChannel.leave();
-      });
-  });
-  }
+      let dispatcher = await connection.playFile('./img/botsound.mp3', {volume: 0.9,});
+      await dispatcher.on('end', function () { 
 
-      
-      setTimeout(function(){ 
-        if (!voiceChannel) return message.reply('**אם אתה קורא לעצמך גבר... כנס לערוץ שיחה ותרשום את זה שוב**')(); 
-    }, 3000);
-      if (!voiceChannel) return message.reply('**אם אתה קורא לעצמך גבר... כנס לערוץ שיחה ותרשום את זה שוב**')
+          setTimeout(function () { voiceChannel.leave();}, 5000);
+
+          for (let member of voiceChannel.members) {member[1].setMute(false)}
+      });
+
+      });
+
+  }
+  
+       let timer = 10000;
+       if (!voiceChannel) return message.reply('**ככה אתה מקלל, אתה יודע מה, אם אתה כזה גבר כנס לשיחה ותכתוב את זה שוב**')
+       for (let member of voiceChannel.members) {member[1].setMute(true)}
+       for (let member of voiceChannel.members) {member[1].setDeaf(false)}
+
        voiceChannel.join()
-      
+       
       const embed2 = new Discord.RichEmbed()
       .setTitle('שים אוזניות ופול ווליום\n יש לנו שיחת נהלים קצרה...')
       .setColor("#FF0000")
-      message.channel.send(embed2);
-
-      setTimeout(function () { play(voiceChannel); }, 30000);
+      .addField('🔇')
+      .setTimestamp()
+      
+      message.channel.send(embed2).then(message => message.delete(120000)).catch(console.error);
+     
+      setTimeout(function () { play(voiceChannel); }, timer);
 
 };
 
 exports.conf = {
   enabled: true,
-  aliases: ['מפגר','טמבל','שתוק'],
+  aliases: ['שתוק'],
   guildOnly: true,
   permLevel: 'User'
 }
 
 exports.help = {
-  name: 'שתוק',
+  name: 'פליי',
   category: 'כיף',
-  description: 'מצטרף אליך לשיחה ומשמיע לך קצת כללים',
+  description: 'כנס לשיחה ותרשום שתוק אם אתה גבר',
   usage: 'שתוק'
 }
