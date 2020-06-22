be93c763ce727e34524759cf33af64d958a7772a
amiboko <56851141+amiboko@users.noreply.github.com>
2020-06-22 14:39:10 +0300

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,12 +1,12 @@
 module.exports = async (client, messageReaction, user) => {
 
     const message = messageReaction.message;
-    const channel = message.guild.channels.find(c => c.name === 'general');
+    const channel = message.guild.channels.find(c => c.name === '𝓡𝓸𝓵𝓮𝓼');
     const member = message.guild.members.get(user.id);
     if (member.user.bot) return;
     if (!channel) return
 
-    const a = message.guild.roles.get('671631357725638656'); // Moderator
+    const a = message.guild.roles.get('671635962228637696'); // Moderator
     // const b = message.guild.roles.get('485987998165499914'); // Administrator
     // const c = message.guild.roles.get('482192667766423561'); // Developer
 
