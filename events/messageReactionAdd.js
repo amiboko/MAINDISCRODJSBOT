8c98c4360e765c26e66731c082a490b2829322e6
amiboko <56851141+amiboko@users.noreply.github.com>
2020-06-22 14:32:25 +0300

    Create messageReactionAdd.js

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,28 @@
+module.exports = async (client, messageReaction, user) => {
+
+    const message = messageReaction.message;
+    const channel = message.guild.channels.find(c => c.name === 'general');
+    const member = message.guild.members.get(user.id);
+    if (member.user.bot) return;
+    if (!channel) return
+
+    const a = message.guild.roles.get('671631357725638656'); // Moderator
+    // const b = message.guild.roles.get('485987998165499914'); // Administrator
+    // const c = message.guild.roles.get('482192667766423561'); // Developer
+
+    if (['🎮'].includes(messageReaction.emoji.name) && message.channel.id === channel.id) {
+        switch (messageReaction.emoji.name) {
+            case '🎮':
+                member.addRole(a).catch(console.error);
+                break;
+            // case '🇧':
+            //     member.addRole(b).catch(console.error);
+            //     break;
+            // case '🇨':
+            //     member.addRole(c).catch(console.error);
+            //     break;
+            default:
+                break;
+        }
+    }
+};
