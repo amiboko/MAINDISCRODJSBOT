201252eafb19dbcc55210b9ee7c8623f4e46619c
amiboko <56851141+amiboko@users.noreply.github.com>
2020-06-23 16:07:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,6 +93,32 @@ client.on('message', (msg) => {
 	antiSpam.message(msg);
 });
 
+
+client.on('raw', packet => {
+  // We don't want this to run on unrelated packets
+  if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+  // Grab the channel to check the message from
+  const channel = client.channels.get(packet.d.channel_id);
+  // There's no need to emit if the message is cached, because the event will fire anyway for that
+  if (channel.messages.has(packet.d.message_id)) return;
+  // Since we have confirmed the message is not cached, let's fetch it
+  channel.fetchMessage(packet.d.message_id).then(message => {
+      // Emojis can have identifiers of name:id format, so we have to account for that case as well
+      const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+      // This gives us the reaction we need to emit the event properly, in top of the message object
+      const reaction = message.reactions.get(emoji);
+      // Adds the currently reacting user to the reaction's users collection.
+      if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
+      // Check which type of event it is before emitting
+      if (packet.t === 'MESSAGE_REACTION_ADD') {
+          client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
+      }
+      if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+          client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
+      }
+  });
+});
+
 client.once('ready', () => {
   const moment = require('moment');
   const CronJob = require('cron').CronJob;
@@ -471,6 +497,27 @@ client.on('presenceUpdate', (oldMember, newMember) => {
 
     // .then(() => client.channels.get(`689067371843158026`)
     // .send(embed2))
+  }
+});
+
+client.on('message', (message) => {
+  if (message.content == 'בטל השתק') {
+    message.delete(5000)
+      let channel = message.member.voiceChannel;
+      if(!channel) return message.channel.send('כנס קודם לערוץ שיחה יחמור').then(message => message.delete(10000).catch());
+      for (let member of channel.members) {
+          member[1].setMute(false)
+          message.channel.send('🔇').then(message => message.delete(10000).catch());
+      }
+   }
+});
+
+client.on("presenceUpdate", (oldGuildMember, newGuildMember) => {
+  const Role = newGuildMember.guild.roles.get('671631357725638656');
+  if (!Role) {return console.error('524302700695912506' + 'שגיאה presenceUpdate')};
+
+  if (newGuildMember.presence.status === 'offline') {
+      newGuildMember.removeRole(Role).catch(e => {console.error(e)});
   }
 });
 
