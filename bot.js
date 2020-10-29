d9ce968946fd4024fb8f56b04b09bfb2304b3f17
amiboko <56851141+amiboko@users.noreply.github.com>
2020-10-29 20:08:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,30 +94,30 @@ client.on('message', (msg) => {
 });
 
 
-client.on('raw', packet => {
-  // We don't want this to run on unrelated packets
-  if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
-  // Grab the channel to check the message from
-  const channel = client.channels.get(packet.d.channel_id);
-  // There's no need to emit if the message is cached, because the event will fire anyway for that
-  if (channel.messages.has(packet.d.message_id)) return;
-  // Since we have confirmed the message is not cached, let's fetch it
-  channel.fetchMessage(packet.d.message_id).then(message => {
-      // Emojis can have identifiers of name:id format, so we have to account for that case as well
-      const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
-      // This gives us the reaction we need to emit the event properly, in top of the message object
-      const reaction = message.reactions.get(emoji);
-      // Adds the currently reacting user to the reaction's users collection.
-      if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
-      // Check which type of event it is before emitting
-      if (packet.t === 'MESSAGE_REACTION_ADD') {
-          client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
-      }
-      if (packet.t === 'MESSAGE_REACTION_REMOVE') {
-          client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
-      }
-  });
-});
+// client.on('raw', packet => {
+//   // We don't want this to run on unrelated packets
+//   if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+//   // Grab the channel to check the message from
+//   const channel = client.channels.get(packet.d.channel_id);
+//   // There's no need to emit if the message is cached, because the event will fire anyway for that
+//   if (channel.messages.has(packet.d.message_id)) return;
+//   // Since we have confirmed the message is not cached, let's fetch it
+//   channel.fetchMessage(packet.d.message_id).then(message => {
+//       // Emojis can have identifiers of name:id format, so we have to account for that case as well
+//       const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+//       // This gives us the reaction we need to emit the event properly, in top of the message object
+//       const reaction = message.reactions.get(emoji);
+//       // Adds the currently reacting user to the reaction's users collection.
+//       if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
+//       // Check which type of event it is before emitting
+//       if (packet.t === 'MESSAGE_REACTION_ADD') {
+//           client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
+//       }
+//       if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+//           client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
+//       }
+//   });
+// });
 
 // client.once('ready', () => {
 //   const moment = require('moment');
@@ -437,6 +437,17 @@ client.on('message', async message => {
     });
 
 
+
+    client.on("presenceUpdate", (oldGuildMember, newGuildMember) => {
+      const Role = newGuildMember.guild.roles.get('671635962228637696');
+      if (!Role) {return console.error('671635962228637696' + 'שגיאה presenceUpdate')};
+    
+      if (newGuildMember.presence.status === 'offline') {
+          newGuildMember.removeRole(Role).catch(e => {console.error(e)});
+      }
+    });
+
+
 client.on('presenceUpdate', (oldMember, newMember) => {
   const guild = newMember.guild;
   const playingRole = guild.roles.find(role => role.id === '671635962228637696');
@@ -445,7 +456,7 @@ client.on('presenceUpdate', (oldMember, newMember) => {
 
   if (newMember.user.bot || oldMember.presence.status !== newMember.presence.status) return;
 
-  const oldGame = oldMember.presence.game && [0, 1].includes(oldMember.presence.game.type) ? true : false;
+  const oldGame = oldMember.presence.activites && [0, 1].includes(oldMember.presence.game.type) ? true : false;
   const newGame = newMember.presence.game && [0, 1].includes(newMember.presence.game.type) ? true : false;
 
   const embed1 = new Discord.RichEmbed()
