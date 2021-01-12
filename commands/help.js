36ab02d6329ca27c0a23273453edcca91de9d33e
amiboko <56851141+amiboko@users.noreply.github.com>
2021-01-12 23:21:09 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,12 +7,16 @@ exports.run = (client, message, args, level) => {
   try {
     if (!args[0]) {
       const embed = new Discord.RichEmbed()
-				.setTitle('עזרה')
-				.setColor(colors.default)
-				.setThumbnail(client.user.avatarURL)
-				.addField('קצת עזרה לא תזיק', `עזרה פקודות: מידע לשימוש בפקודות \n עזרה <שם פקודה>: פרטים לכל פקודה`)
-				.addField('רוצים להזמין עוד משהוא לשרת?', '[CLICK ME](https://discord.gg/RcHes6y)')
-				.setImage("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/s960x960/81214613_1397300473812746_7875231392332775424_o.jpg?_nc_cat=109&_nc_ohc=2mjhu9slCRgAX-QuI0n&_nc_ht=scontent.fhfa2-2.fna&_nc_tp=1002&oh=209fde4a212c3de86dcf387e57fec1c0&oe=5E9ACFDF")
+      .setTitle('GAMERS UNITED RULES')
+      .addField(`תקללו חופשי רק קחו בחשבון שלי מותר הכל`,'➖➖➖➖➖➖➖➖➖➖')
+      .addField(`יש לי פתיל קצר אז בלי חפירות בבקשה`,'➖➖➖➖➖➖➖➖➖➖')
+      .addField(`גזענות לא מתקבלת על הדעת (לא תופס על תיימנים)`,'➖➖➖➖➖➖➖➖➖➖')
+      .addField(`צריכים עזרה? רשמו עזרה או פקודות בערוץ MAIN ותקבלו באהבה`,'➖➖➖➖➖➖➖➖➖➖')
+      .setDescription(`ברוכים הבאים לערוץ שלנו \n כמה כללים חשובים\n`)
+  		.setColor(colors.default)
+			.setThumbnail(client.user.avatarURL)
+			.addField('רוצים להזמין עוד משהוא לשרת?', '[CLICK ME](https://discord.gg/RcHes6y)')
+			.setImage("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/s960x960/81214613_1397300473812746_7875231392332775424_o.jpg?_nc_cat=109&_nc_ohc=2mjhu9slCRgAX-QuI0n&_nc_ht=scontent.fhfa2-2.fna&_nc_tp=1002&oh=209fde4a212c3de86dcf387e57fec1c0&oe=5E9ACFDF")
 
       message.channel.send(embed)
     } else {
