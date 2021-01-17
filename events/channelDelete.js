e28ef52c0cf10261f2f6274ec2934ebe3a4dfc78
amiboko <56851141+amiboko@users.noreply.github.com>
2021-01-17 22:20:23 +0200

    Update channelDelete.js

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -4,19 +4,20 @@ const colors = require('../lib/colors.json')
 module.exports = (client, channel) => {
   	const settings = client.getSettings(channel.guild.id)
   	if (settings.logChannelUpdates == 'true') {
-    if (settings.modLogChannel && channel.guild.channels.find(c => c.name == settings.modLogChannel)) {
-      const modLogChannel = channel.guild.channels.find(c => c.name == settings.modLogChannel)
-      if (!modLogChannel.permissionsFor(channel.guild.me).has('VIEW_CHANNEL')) return
-      if (!modLogChannel.permissionsFor(channel.guild.me).has('SEND_MESSAGES')) return
+    if (settings.modLogChannel2 && channel.guild.channels.find(c => c.name == settings.modLogChannel2)) {
+      const modLogChannel2 = channel.guild.channels.find(c => c.name == settings.modLogChannel2)
+      if (!modLogChannel2.permissionsFor(channel.guild.me).has('VIEW_CHANNEL')) return
+      if (!modLogChannel2.permissionsFor(channel.guild.me).has('SEND_MESSAGES')) return
 
       const embed = new Discord.RichEmbed()
         .setAuthor('🗑️ ערוץ נמחק 🗑️')
         .setColor(colors.red)
         .setDescription(`\`${channel.name}\``)
         .setFooter('הודעה אוטומטית')
+        .setThumbnail(client.user.avatarURL)
         .setTimestamp()
 
-      modLogChannel.send(embed)
+      modLogChannel2.send(embed)
     }
   	}
 }
