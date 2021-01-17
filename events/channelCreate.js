415e1d92b3e52e49c9a113931ab1222d9755e6a0
amiboko <56851141+amiboko@users.noreply.github.com>
2021-01-17 22:20:22 +0200

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -5,18 +5,19 @@ module.exports = (client, channel) => {
   if (channel.type == "dm") return
   	const settings = client.getSettings(channel.guild.id)
   	if (settings.logChannelUpdates == 'true') {
-      if (settings.modLogChannel && channel.guild.channels.find(c => c.name == settings.modLogChannel)) {
-      const modLogChannel = channel.guild.channels.find(c => c.name == settings.modLogChannel)
-      if (!modLogChannel.permissionsFor(channel.guild.me).has('VIEW_CHANNEL')) return
-      if (!modLogChannel.permissionsFor(channel.guild.me).has('SEND_MESSAGES')) return
+      if (settings.modLogChannel2 && channel.guild.channels.find(c => c.name == settings.modLogChannel2)) {
+      const modLogChannel2 = channel.guild.channels.find(c => c.name == settings.modLogChannel2)
+      if (!modLogChannel2.permissionsFor(channel.guild.me).has('VIEW_CHANNEL')) return
+      if (!modLogChannel2.permissionsFor(channel.guild.me).has('SEND_MESSAGES')) return
       const embed = new Discord.RichEmbed()
         .setAuthor('🔨 ערוץ נוצר 🔨')
         .setColor(colors.green)
         .setDescription(`${channel}`)
         .setFooter('הודעה אוטומטית')
+        .setThumbnail(client.user.avatarURL)
         .setTimestamp()
 
-      modLogChannel.send(embed)
+      modLogChannel2.send(embed)
     }
   }
 }
