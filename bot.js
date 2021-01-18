28e3acb069d57a3ba82de22aaebcaf84be0ea732
amiboko <56851141+amiboko@users.noreply.github.com>
2021-01-18 22:38:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,27 +94,11 @@ client.on('message', (msg) => {
 });
 
 
-client.on("presenceUpdate", (oldGuildMember, newGuildMember) => {
-  if (newGuildMember.guild.id !== "GuildID") {return false};
-  const Role = newGuildMember.guild.roles.get("771450742070444042");
-  const team1members = Role.members;
-  if (!Role) {return console.error("No role found.")};
-  // const chan1 = message.guild.channels.find((channel) => channel.name === "Team 1")
-  
-  if (newGuildMember.presence.status === 'offline')
-  team1members.forEach((member) => {
-    member.removeRole(Role);
-  });
-  
-});
-
-
 client.on('presenceUpdate', (oldMember, newMember) => {
   const guild = newMember.guild;
   const playingRole = guild.roles.find(role => role.id === '771450742070444042');
 
-  if (newMember.user.bot) return;
-  // if (newMember.user.bot || oldMember.presence.status !== newMember.presence.status) return;
+  if (newMember.user.bot || oldMember.presence.status !== newMember.presence.status) return;
 
   const oldGame = oldMember.presence.game && [0, 1].includes(oldMember.presence.game.type) ? true : false;
   const newGame = newMember.presence.game && [0, 1].includes(newMember.presence.game.type) ? true : false;
@@ -153,6 +137,21 @@ client.on('raw', packet => {
           client.emit('messageReactionRemove', reaction, client.users.get(packet.d.user_id));
       }
   });
+});
+
+client.on("presenceUpdate", (oldGuildMember, newGuildMember) => {
+  if (newGuildMember.guild.id !== "GuildID")
+  const Role = newGuildMember.guild.roles.get("771450742070444042");
+  if (!Role) {return console.error("No role found.")};
+
+  if (newGuildMember.presence.status == "offline") {
+      newGuildMember.removeRole(Role).catch(e => {console.error(e)});
+      Role.members.forEach((member, i) => { // Looping through the members of Role.
+        setTimeout(() => {
+            member.roles.remove(Role); // Removing the Role.
+        }, i * 1000);
+    });
+  } 
 });
 
 // client.once('ready', () => {
