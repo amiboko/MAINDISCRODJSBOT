bf536bf4ff59b1307c63934416700ff12710d1aa
amiboko <56851141+amiboko@users.noreply.github.com>
2021-05-06 13:05:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,16 +69,16 @@ const antiSpam = new AntiSpam({
 	warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
 	kickThreshold: 10, // Amount of messages sent in a row that will cause a kick.
 	banThreshold: 12, // Amount of messages sent in a row that will cause a ban.
-	muteThreshold: 5, // Amount of messages sent in a row that will cause a mute.
-	maxInterval: 3000, // Amount of time (in milliseconds) in which messages are considered spam.
+	muteThreshold: 2, // Amount of messages sent in a row that will cause a mute.
+	maxInterval: 1500, // Amount of time (in milliseconds) in which messages are considered spam.
 	warnMessage: '{@user}, עקב ספאם הוספתי אותך לרשימה השחורה, במידה ותמשיך להספים תעוף אוטומטית,', // Message that will be sent in chat upon warning a user.
 	kickMessage: '**{user_tag}** קיבל קיק בעקבות ספאם', // Message that will be sent in chat upon kicking a user.
 	banMessage: '**{user_tag}** קיבל באן בעקבות ספאם', // Message that will be sent in chat upon banning a user.
 	muteMessage: '**{user_tag}** הושתק בעקבות ספאם', // Message that will be sent in chat upon muting a user.
-	maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesWarning: 2, // Amount of duplicate messages that trigger a warning.
 	maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
 	maxDuplicatesBan: 12, // Amount of duplicate messages that trigger a warning.
-	maxDuplicatesMute: 9, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesMute: 1, // Amount of duplicate messages that trigger a warning.
 	exemptPermissions: [ 'ADMINISTRATOR'], 
 	ignoreBots: true, // Ignore bot messages.
 	verbose: true, // Extended Logs from module.
