867c2ac09888b7db556ed69d6033de4cc5a17492
amiboko <56851141+amiboko@users.noreply.github.com>
2021-05-13 13:24:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -193,7 +193,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
       newUserChannel.join().then(connection => {
       const dispatcher = connection.playFile('./img/taverna.mp3', {volume: 1.0});
        dispatcher.on('end', function () { 
-          setTimeout(function () { newUserChannel.leave() }, 50000);
+          setTimeout(function () { newUserChannel.leave() }, 10000);
         });
       console.log("Successfully connected.");
     }).catch(e => {
